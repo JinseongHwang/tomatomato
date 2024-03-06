48a56765d765203b2e35d2c8a1f580e5e84b5741
JinseongHwang <jinseong.dev@gmail.com>
2024-03-06 15:17:56 +0900

    feat: init project

---
+++ src/main/kotlin/com/kurly/ft/tomatomato/TomatomatoApplication.kt
@@ -0,0 +1,11 @@
+package com.kurly.ft.tomatomato
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TomatomatoApplication
+
+fun main(args: Array<String>) {
+    runApplication<TomatomatoApplication>(*args)
+}
---
+++ src/test/kotlin/com/kurly/ft/tomatomato/TomatomatoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kurly.ft.tomatomato
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TomatomatoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
